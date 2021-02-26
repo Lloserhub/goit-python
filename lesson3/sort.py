c76bf25de8868e663ae841d8a58c1c0410d37000
Lloserhub <55021786+Lloserhub@users.noreply.github.com>
2021-02-26 15:51:32 +0200

    hw3 update

--- lesson3/sort.py
+++ lesson3/sort.py
@@ -1,33 +1,76 @@
 import os
 import sys
 
-# path содержит первый аргумент, считаем, что это валидный адрес в файловой системе
-path = sys.argv[1]
-print(f"Start in {path}")
+# Creating lists of possible extensions
+IMAGES = ['JPEG', 'PNG', 'JPG', 'SVG']
+VIDEOS = ['AVI', 'MP4', 'MOV', 'MKV']
+DOCUMENTS = ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX']
+MUSIC = ['MP3', 'OGG', 'WAV', 'AMR']
+ARCHIVES = ['ZIP', 'GZ', 'TAR']
 
-# files - это список имен файлов и папок в path.
-files = os.listdir(path)
+# Creating lists of files
+images_list = list()
+videos_list = list()
+documents_list = list()
+music_list = list()
+archives_list = list()
+unknown_extension_list = list()
+folders_list = list()
 
-text_files = []
-video_files = []
-image_files = []
-music_files = []
-unknown_files = []
-
-for file in files:
-    if file.endswith('.doc') or file.endswith('.docx') or file.endswith('.txt'):
-        text_files.append(file)
-    elif file.endswith('.avi') or file.endswith('.mp4') or file.endswith('.mov'):
-        video_files.append(file)
-    elif file.endswith('.jpeg') or file.endswith('.png') or file.endswith('.jpg'):
-        image_files.append(file)
-    elif file.endswith('.mp3') or file.endswith('.ogg') or file.endswith('.wav') or file.endswith('.amr'):
-        music_files.append(file)
+# Print all files with extensions from a specific folder if there are any
+def print_files(folder):
+    # Check if we have any files
+    if len(folder) > 0:
+        print('Files in folder: ')
+        for file in folder:
+            print(file)
     else:
-        unknown_files.append(file)
+        print(f'No files detected in {os.listdir(sys.argv[1])}.')
 
-print(f'Text files >> {text_files}')
-print(f'Video files >> {video_files}')
-print(f'Image files >> {image_files}')
-print(f'Music files >> {music_files}')
-print(f'Unknown files >> {unknown_files}')
+# Print all extensions 
+def print_extensions(file_list):
+    extensions_set = set()
+    try:
+        for file in file_list:
+            extensions_set.add(file.split('.')[1].upper())
+    except IndexError:
+        pass
+    print(f'All extensions: {extensions_set}')
+
+# Sorting files by category
+def sort_files(folder):
+    for file in folder:
+        try: 
+            if file.split('.')[1].upper() in IMAGES:
+                images_list.append(file)
+            elif file.split('.')[1].upper in VIDEOS:
+                videos_list.append(file)
+            elif file.split('.')[1].upper in DOCUMENTS:
+                documents_list.append(file)
+            elif file.split('.')[1].upper in MUSIC:
+                music_list.append(file)
+            elif file.split('.')[1].upper in ARCHIVES:
+                archives_list.append(file)
+            else:
+                unknown_extension_list.append(file)
+        except IndexError:
+            folders_list.append(file)
+    print(f'Images: {images_list}')
+    print(f'Videos: {videos_list}')
+    print(f'Documents: {documents_list}')
+    print(f'Music: {music_list}')
+    print(f'Archives: {archives_list}')
+    print(f'Unknown fextension files: {unknown_extension_list}')
+    print(f'Folders: {folders_list}')
+
+            
+def main():
+    path = sys.argv[1]
+
+    print_files(os.listdir(path))
+    print_extensions(os.listdir(path))
+
+    sort_files(os.listdir(path))
+
+if __name__ == '__main__':
+    main()
